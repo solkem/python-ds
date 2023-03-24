f19f62cd12eb2a7d05d6cd5568c01f49c30cbdd9
Solomon Kembo <solomonkembo@gmail.com>
2023-03-24 14:49:56 -0400

    remove flatten

--- data_structures/array/flatten_array.py
+++ data_structures/array/flatten_array.py
@@ -5,9 +5,14 @@ lst = [1,2,3,4,[5,6,7,8],[9,10]]
 
 flat = []
 
-for sub in lst:
-  if isinstance(sub, list):
-    flat.extend(sub)
-  else:
-    flat.append(sub)
+def flatten(lst):
+  for sub in lst:
+    if isinstance(sub, list):
+      flat.extend(sub)
+    else:
+      flat.append(sub)
+
+  print(flat)
   
+
+print(flatten(lst))
