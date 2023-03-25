f5281ad10409e26d9b7653bfe41293dfb868c216
Solomon Kembo <solomonkembo@gmail.com>
2023-03-25 13:31:07 -0400

    find peak

---
+++ data_structures/array/find_peak.py
@@ -0,0 +1,9 @@
+def find_peak(arr):
+    for i,n in enumerate(arr):
+        if i==0 or i==len(arr)-1:
+            pass
+        else:
+            if arr[i-1] < arr[i] and arr[i+1]<arr[i]:
+                return arr[i]
+arr=[5,10,20,25,30,24,78]
+print(find_peak(arr))
