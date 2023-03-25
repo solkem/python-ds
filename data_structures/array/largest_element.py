c18999dbdee5e3760ba7ea46951f76bbb5de9898
Solomon Kembo <solomonkembo@gmail.com>
2023-03-25 12:02:54 -0400

    largest number

--- data_structures/array/largest_element.py
+++ data_structures/array/largest_element.py
@@ -1,11 +1,11 @@
 import sys
 
 def largest(arr):
-    l = -sys.maxsize
+    largest_num = -sys.maxsize
     for i in arr:
-        if i > l:
-            l = i
-    print(l)
+        if i > largest_num:
+            largest_num = i
+    print(largest_num)
 
 arr = [1,2,3,4,5,6,7,8,2,4,5,7,34,56,23,1234,134,57,67,345]
 
